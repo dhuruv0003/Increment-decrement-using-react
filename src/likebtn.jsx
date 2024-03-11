fd59ebe0ddfc33271e12f9263bdf6b7e467f86a4
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 15:07:45 +0530

    "new commit"

--- src/likebtn.jsx
+++ src/likebtn.jsx
@@ -2,17 +2,20 @@ import { useState } from "react";
 
 export default function Likebutton() {
     let ncolor={color:"red",cursor:"pointer", fontSize:"3rem" }
-    let [like,setlike]=useState(false)
-    let displaylike= (!like)?
-                     <i className="fa-regular fa-heart" style={ncolor}></i> 
-                     : <i class="fa-solid fa-heart" style={ncolor}></i>
+    let [liked,setlike]=useState(false)
+    let displaylike= (!liked)?
+                     <i className="fa-regular fa-heart" style={ncolor}></i> :
+                     <i class="fa-solid fa-heart" style={ncolor}></i>
     function handlelike(){
-        setlike(!like)
+        setlike(!liked)
     }
     return (
         <div>
             <p onClick={handlelike}>
                 {displaylike}
+            </p>
+            <p onClick={handlelike}>
+                {liked.toString()}
             </p>
         </div>
     );
