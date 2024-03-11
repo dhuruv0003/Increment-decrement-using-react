fc6588504ba1e52db85a77c9dc06369827c65bb5
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 14:40:40 +0530

    "new commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Counter from './counter'
+import LikeButton from './likebtn'
+
 function App() {
    return (
     <>
       <Counter></Counter>
       <h1>Learning states</h1>
+      <LikeButton/>
     </>
   )
 }
---
+++ src/likebtn.jsx
@@ -0,0 +1,9 @@
+export default function Likebutton() {
+    return (
+        <div>
+            <p>
+                <i className="fa-regular fa-heart" ></i>
+            </p>
+        </div>
+    );
+}
