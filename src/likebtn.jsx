e080f8f0ac317b4aeb72d5cf26a80c706f2a4db5
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 14:47:03 +0530

    "new commit"

--- src/likebtn.jsx
+++ src/likebtn.jsx
@@ -1,8 +1,11 @@
+
 export default function Likebutton() {
+    let ncolor={color:"red",cursor:"pointer", fontSize:"3rem" }
+
     return (
         <div>
             <p>
-                <i className="fa-regular fa-heart" ></i>
+            <i className="fa-regular fa-heart" style={ncolor}></i>
             </p>
         </div>
     );
